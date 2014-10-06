477358516bfb1b4f754a0211be03d1e8d8b61176
joe5656123 <joe5656123@aol.com>
2014-10-06 17:36:08 -0400

    This is a summary
    Desc.

--- Pizza.java
+++ Pizza.java
@@ -30,11 +30,37 @@ public class Pizza extends Entree {
 	}
 
 	public float getPriceWithAddons() {
-		float output = (this.getPrice() == 0) ? 2.99f : this.getPrice();
+		float output = (this.getPrice() == 0) ? 14.99f : this.getPrice();
 		for (Toppings t : this._toppings) {
 			output += t.getPrice();
 		}
 		return output;
 	}
 
+	public String getDescription() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.getName() + "\n");
+		for (Toppings c : _toppings) {
+			sb.append("* " + c.toString() + "\n");
+		}
+		return sb.toString();
+	}
+	public String getSummary() {
+		Toppings[] c = this._toppings.toArray(new Toppings[this._toppings.size()]);
+		String summery = "A " + this.getName() + " with: ";
+		if (c.length == 2) {
+			summery += c[0] + " and " + c[1] + ".";
+		} else if (c.length > 1) {
+			for (int i = 0, size = c.length; i < size; i++) {
+				if (i == size - 1) {
+					summery += "and " + c[i].toString() + ".";
+				} else {
+					summery += c[i].toString() + ", ";
+				}
+			}
+		} else {
+			summery += c[0] + ".";
+		}
+		return summery;
+	}
 }
